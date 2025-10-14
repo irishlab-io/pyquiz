72ea8ae7f3be881085b51bee340d9072f7b7ba5e
Simon Harvey <simon.harvey.86@gmail.com>
2025-10-13 20:57:53 -0400

    feat: minor code update

--- apps/main.py
+++ apps/main.py
@@ -6,10 +6,10 @@ from quiz import Quiz
 def main():  
     """Entry point for the quiz application."""
     quiz = Quiz()  
-
-    quiz.run()
-   
     
+    quiz.run()
+    
+     
 if __name__ == "__main__":
     main()
-    
+      
